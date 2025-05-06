f3fcb9a2a79db31e2dc2d831a42f71356288e3ce
levibecker <itik-nfte@web.de>
2025-05-06 11:19:14 +0200

    mes

--- src/models/vehicles/vehicle.cpp
+++ src/models/vehicles/vehicle.cpp
@@ -10,6 +10,7 @@ void Vehicle::addCoach(Coach const& coach)
 {
     // TODO
     
+
     coaches.push_back(coach);
     reservations.push_back(std::unordered_map<std::string, std::vector<Reservation>>());
 
@@ -18,11 +19,40 @@ void Vehicle::addCoach(Coach const& coach)
 void Vehicle::addStop(std::string const& stop)
 {
     // TODO
+    
+
+    route.push_back(stop);
+
 }
 
 void Vehicle::addReservation(size_t coachIndex, std::string const& seatId, Reservation const& reservation)
 {
     // TODO
+
+    // Überprüfen Sie, ob der Wagen-Index gültig ist.
+    // Falls ja, fügen Sie die Reservierung für den Sitzplatz zur Map
+    // der Reservierungen für diesen Wagen hinzu (also zu `reservations[coachIndex]`).
+    // Dabei sollte auch überprüft werden, ob die Map bereits einen Eintrag für den Sitzplatz hat.
+
+    if (coachIndex < reservations.size()){
+
+        // Eine leere Liste von Reservierungen für den Sitzplatz erstellen,
+        // falls noch keine Reservierungen vorhanden sind.
+
+        auto& coach_res = reservations[coachIndex];
+        if (!coach_res.contains(seatId)) {
+            coach_res[seatId] = std::vector<Reservation>();
+        }
+
+        // In diesem Schritt wird die Reservierung hinzugefügt
+
+        auto& seat_res = reservations[coachIndex][seatId];
+        insert_reservation(reservation, seat_res, route);
+
+    } else {
+        std::cout << "Reservierung fehlgeschlagen" << std::endl;
+    }
+
 }
 
 void Vehicle::showAllDisplays() const
